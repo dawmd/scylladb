4aa5c5ba0b726b37974a56ca71fb4bbc73d1889b
Dawid Medrek <dawid.medrek@scylladb.com>
2023-10-20 18:34:49 +0200

    some changes

--- db/hints/internal/hint_storage.cc
+++ db/hints/internal/hint_storage.cc
@@ -13,6 +13,8 @@
 #include <seastar/core/coroutine.hh>
 #include <seastar/core/file.hh>
 #include <seastar/core/file-types.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/scheduling.hh>
 #include <seastar/core/seastar.hh>
 #include <seastar/core/smp.hh>
 #include <seastar/core/sstring.hh>
@@ -21,10 +23,13 @@
 #include <boost/range/adaptors.hpp>
 
 // Scylla includes.
+#include "db/commitlog/commitlog_entry.hh"
 #include "db/hints/internal/hint_logger.hh"
-#include "seastar/core/future.hh"
+#include "seastar/core/semaphore.hh"
+#include "seastar/util/later.hh"
 #include "utils/disk-error-handler.hh"
 #include "utils/lister.hh"
+#include "utils/runtime.hh"
 
 // STD.
 #include <concepts>
@@ -271,6 +276,176 @@ future<> rebalance_hints(fs::path hint_directory) {
     // Remove the directories of shards that are not present anymore.
     // They should not have any segments by now.
     co_await remove_irrelevant_shards_directories(hint_directory);
+}
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+// The reason why we use std::string rather than seastar::sstring is simple -- commitlog uses it.
+static const std::string HINT_FILENAME_PREFIX = {"HintsLog" + commitlog::descriptor::SEPARATOR};
+static constexpr std::chrono::seconds HINT_FILE_WRITE_TIMEOUT = std::chrono::seconds(2);
+
+hint_endpoint_storage::hint_endpoint_storage(const extensions& exts, seastar::scheduling_group sched_group) noexcept
+    : _write_rp(this_shard_id(), std::chrono::duration_cast<std::chrono::milliseconds>(
+            runtime::get_boot_time().time_since_epoch()).count())
+    , _sched_group(sched_group)
+    , _flush_timer([this] {
+        // Unfortunately, the callback cannot return a future, so we need to discard it here.
+        (void) maybe_flush_hints();
+    })
+    , _extensions(exts)
+{}
+
+future<> hint_endpoint_storage::start(const fs::path endpoint_hint_directory) {
+    co_await io_check([name = std::string_view{endpoint_hint_directory.native()}] {
+        return recursive_touch_directory(name);
+    });
+
+    commitlog::config cfg;
+
+    cfg.commit_log_location = endpoint_hint_directory.native();
+    cfg.commitlog_segment_size_in_mb = HINT_SEGMENT_SIZE_IN_MB;
+    cfg.commitlog_total_space_in_mb = MAX_HINT_SIZE_PER_ENDPOINT_IN_MB;
+    cfg.fname_prefix = HINT_FILENAME_PREFIX;
+    cfg.extensions = std::addressof(_extensions.get());
+
+    // HH leaves segments on disk after commitlog shutdown, and later reads
+    // them when commitlog is re-created. This is expected to happen regularly
+    // during standard HH workload, so no need to print a warning about it.
+    cfg.warn_about_segments_left_on_disk_after_shutdown = false;
+    // Allow going over the configured size limit of the commitlog
+    // (resource_manager::max_hints_per_ep_size_mb). The commitlog will
+    // be more conservative with its disk usage when going over the limit.
+    // On the other hand, HH counts used space using the space_watchdog
+    // in resource_manager, so its redundant for the commitlog to apply
+    // a hard limit.
+    cfg.allow_going_over_size_limit = true;
+    // The API for waiting for hint replay relies on replay positions
+    // monotonically increasing. When there are no segments on disk,
+    // by default the commitlog will calculate the first segment ID
+    // based on the boot time. This may cause the following sequence
+    // of events to occur:
+    //
+    // 1. Node starts with empty hints queue
+    // 2. Some hints are written and some segments are created
+    // 3. All hints are replayed
+    // 4. Hint sync point is created
+    // 5. Commitlog instance gets re-created and resets it segment ID counter
+    // 6. New hint segment has the first ID as the first (deleted by now) segment
+    // 7. Waiting for the sync point commences but resolves immediately
+    //    before new hints are replayed - since point 5., `_last_written_rp`
+    //    and `_sent_upper_bound_rp` are not updated because RPs of new
+    //    hints are much lower than both of those marks.
+    //
+    // In order to prevent this situation, we override the base segment ID
+    // of the newly created commitlog instance - it should start with an ID
+    // which is larger than the segment ID of the RP of the last written hint.
+    cfg.base_segment_id = _write_rp.base_id();
+
+    _commitlog = std::make_unique<commitlog>(co_await commitlog::create_commitlog(std::move(cfg)));
+}
+
+future<> hint_endpoint_storage::read_old_hints(const fs::path endpoint_hint_directory) {
+    auto segment_vector = co_await _commitlog->get_segments_to_replay();
+    if (segment_vector.empty()) [[unlikely]] {
+        co_return;
+    }
+
+    std::vector<std::pair<segment_id_type, sstring>> segments{};
+    segments.reserve(segment_vector.size());
+
+    const auto my_shard_id = this_shard_id();
+    for (auto& segment : segment_vector) {
+        commitlog::descriptor desc{segment, HINT_FILENAME_PREFIX};
+        if (replay_position{desc}.shard_id() == my_shard_id) {
+            segments.emplace_back(desc.id, std::move(segment));
+        } else {
+            _foreign_segment_list.emplace_back(std::move(segment));
+        }
+    }
+
+    // Sort segments that belonged to this shard based on their segment IDs,
+    // which should correspond to the chronological order.
+    std::sort(segments.begin(), segments.end());
+    for (auto& [_, segment] : segments) {
+        _segment_list.emplace_back(std::move(segment));
+    }
+}
+
+future<> hint_endpoint_storage::store_hint(schema_ptr s, lw_shared_ptr<const frozen_mutation> fm) noexcept
+{
+    const auto mutex_lock = co_await seastar::get_units(_file_update_mutex, 1);
+    commitlog_entry_writer cew{s, *fm, commitlog::force_sync::no};
+
+    rp_handle rph = co_await _commitlog->add_entry(s->id(), cew, timeout_clock_type::now() + HINT_FILE_WRITE_TIMEOUT);
+    _write_rp = std::max(_write_rp, rph.release());
+}
+
+struct hint_file_reading_stopped {};
+
+future<> hint_endpoint_storage::read_hints(hint_reader_type reader_function) {
+    if (_foreign_segment_list.empty() && _segment_list.empty()) {
+        co_return;
+    }
+
+    try {
+        while (true) {
+            auto& slist = !_foreign_segment_list.empty()
+                    ? _foreign_segment_list
+                    : _segment_list;
+            
+            co_await read_one_hint_file(slist.front(), reader_function);
+            
+            slist.pop_front();
+            _read_rp = 0;
+        }
+    } catch (const hint_file_reading_stopped&) {/* ignore */}
+}
+
+future<> hint_endpoint_storage::read_one_hint_file(const sstring& segment_name, hint_reader_type& reader) {
+    bool modify_rp = true;
+    
+    co_await commitlog::read_log_file(segment_name, HINT_FILENAME_PREFIX,
+            coroutine::lambda([&] (commitlog::buffer_and_replay_position bufrp) mutable -> future<> {
+        auto& buffer = bufrp.buffer;
+        auto& rp = bufrp.position;
+
+        const auto result = co_await reader(hint_entry_reader{buffer}, buffer.size_bytes());
+
+        switch (result) {
+        case hint_reader_result::CONTINUE_FORGET:
+            if (modify_rp) {
+                _read_rp = std::max(_read_rp, rp);
+            }
+            break;
+        case hint_reader_result::CONTINUE_KEEP:
+            modify_rp = false;
+            break;
+        case hint_reader_result::STOP_FORGET:
+            [[fallthrough]];
+        case hint_reader_result::STOP_KEEP:
+            throw hint_file_reading_stopped{};
+        }
+
+        co_await maybe_notify_waiters();
+    }));
+}
+
+future<> hint_endpoint_storage::maybe_flush_hints() {
+    // For now, we don't really check anything. It might even be unnecessary in general.
+    co_await flush_hints();
+}
+
+future<> hint_endpoint_storage::flush_hints() {
+    co_await _commitlog->sync_all_segments();
+    _flush_timer.rearm(timeout_clock_type::now() + HINT_FILE_WRITE_TIMEOUT);
+}
+
+future<> hint_endpoint_storage::maybe_notify_waiters() {
+    // TO BE IMPLEMENTED
+    co_return;
 }
 
 } // namespace internal
--- db/hints/internal/hint_storage.hh
+++ db/hints/internal/hint_storage.hh
@@ -8,14 +8,24 @@
  */
 #pragma once
 
+// Seastar features.
+#include <seastar/core/lowres_clock.hh>
+#include <seastar/core/scheduling.hh>
+#include <seastar/core/semaphore.hh>
+#include <seastar/core/timer.hh>
+#include <seastar/util/noncopyable_function.hh>
+
 // Scylla includes.
 #include "db/commitlog/commitlog.hh"
 #include "db/commitlog/commitlog_entry.hh"
 #include "db/hints/internal/common.hh"
+#include "mutation/frozen_mutation.hh"
+#include "tracing/trace_state.hh"
 #include "utils/loading_shared_values.hh"
 
 // STD.
 #include <filesystem>
+#include <utility>
 
 /// This file is supposed to gather meta information about data structures
 /// and types related to storing hints.
@@ -45,6 +55,81 @@ using hint_entry_reader = commitlog_entry_reader;
 /// \param hint_directory A hint directory to rebalance
 /// \return A future that resolves when the operation is complete.
 future<> rebalance_hints(std::filesystem::path hint_directory);
+
+constexpr inline size_t HINT_SEGMENT_SIZE_IN_MB = 32;
+constexpr inline size_t MAX_HINT_SIZE_PER_ENDPOINT_IN_MB = 128;
+
+class hint_endpoint_storage {
+public:
+    enum class hint_reader_result {
+        CONTINUE_FORGET,
+        CONTINUE_KEEP,
+        STOP_FORGET,
+        STOP_KEEP
+    };
+
+    using hint_size_type = size_t;
+    using hint_reader_type = noncopyable_function<future<hint_reader_result> (hint_entry_reader, hint_size_type)>;
+
+private:
+    using flush_clock_type = seastar::lowres_clock;
+    static_assert(noexcept(flush_clock_type::now()), "flush_clock_type::now() must be noexcept");
+    using flush_timer_type = timer<flush_clock_type>;
+
+    using timeout_clock_type = seastar::lowres_clock;
+    static_assert(noexcept(timeout_clock_type::now()), "timeout_clock_type::now() must be noexcept");
+
+    using segment_list = std::list<sstring>;
+
+private:
+    std::unique_ptr<commitlog> _commitlog = nullptr;
+
+    // The position of the last written hint to the underlying commitlog instance.
+    // New hints should be written past it.
+    replay_position _write_rp{};
+    // The position of the first hint that should be read next time.
+    replay_position _read_rp{};
+
+    segment_list _segment_list{};
+    segment_list _foreign_segment_list{};
+
+    // This scheduling group shouldn't affect the performance of mutations. Storing a hint is performed
+    // asynchronously to mutating. Though `store_hint` returns a future, it should be immediately
+    // discarded by the caller.
+    seastar::scheduling_group _sched_group;
+
+    // Timer used for regular flushing hints to disk.
+    flush_timer_type _flush_timer;
+    seastar::semaphore _file_update_mutex{1};
+    // Used for creating and read from the commitlog instance.
+    std::reference_wrapper<const extensions> _extensions;
+
+public:
+    hint_endpoint_storage(const extensions& exts, seastar::scheduling_group sched_group) noexcept;
+    
+    hint_endpoint_storage(const hint_endpoint_storage&) = delete;
+    hint_endpoint_storage& operator=(const hint_endpoint_storage&) = delete;
+    
+    hint_endpoint_storage(hint_endpoint_storage&&) noexcept = delete;
+    hint_endpoint_storage& operator=(hint_endpoint_storage&&) noexcept = delete;
+    
+public:
+    future<> start(const std::filesystem::path endpoint_hint_directory);
+    future<> read_old_hints(const std::filesystem::path endpoint_hint_directory);
+
+    future<> store_hint(schema_ptr, lw_shared_ptr<const frozen_mutation>) noexcept;
+    future<> read_hints(hint_reader_type reader_functor);
+
+private:
+    future<> read_one_hint_file(const sstring& segment_name, hint_reader_type& reader_functor);
+    future<> maybe_flush_hints();
+    future<> flush_hints();
+    future<> maybe_notify_waiters();
+};
+
+class hint_storage {
+private:
+};
 
 } // namespace internal
 } // namespace db::hints
