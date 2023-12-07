0f2ea8be6b352ed9dad3ec78e3a5965a0ebbfae6
Dawid Medrek <dawid.medrek@scylladb.com>
2023-12-07 11:31:33 +0100

    cql-test

--- test/lib/cql_test_env.cc
+++ test/lib/cql_test_env.cc
@@ -868,7 +868,7 @@ private:
             _ss.local().set_group0(group0_service, raft_topology_change_enabled);
 
             try {
-                _ss.local().join_cluster(_sys_dist_ks, _proxy).get();
+                _ss.local().join_cluster(_sys_dist_ks, _proxy, false).get();
             } catch (std::exception& e) {
                 // if any of the defers crashes too, we'll never see
                 // the error
