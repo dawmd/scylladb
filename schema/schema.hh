5b92c0fc5d9067a31192a919c0b91c42a66d6278
Dawid Medrek <dawid.medrek@scylladb.com>
2024-08-26 16:51:10 +0200

    schema/schema: Fix indentation

--- schema/schema.hh
+++ schema/schema.hh
@@ -915,7 +915,7 @@ public:
      * Index or Local Index).
      *
      * When `with_internals` is true, the description is extended with table's id and dropped columns.
-     * The dropped columns are present in column definitions and also the `ALTER DROP` statement 
+     * The dropped columns are present in column definitions and also the `ALTER DROP` statement
      * (and `ALTER ADD` if the column has been re-added) to the description.
      */
     virtual std::ostream& describe(replica::database& db, std::ostream& os, bool with_internals) const override;
