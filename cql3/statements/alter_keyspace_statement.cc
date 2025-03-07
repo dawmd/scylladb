3d136e5cb399e16ecb1a90ac405fb2efcb1ebe27
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-03-07 20:19:12 +0100

    cql3: Ensure that CREATE and ALTER never lead to RF-rack-invalid keyspaces
    In this commit, we refuse to create or alter a keyspace when that operation
    would make it RF-rack-invalid if the option `rf_rack_valid_keyspaces` is
    enabled.
    
    We provide a test verifying that the changes work as intended.
    
    Fixes scylladb/scylladb#23276

--- cql3/statements/alter_keyspace_statement.cc
+++ cql3/statements/alter_keyspace_statement.cc
@@ -25,6 +25,7 @@
 #include "create_keyspace_statement.hh"
 #include "gms/feature_service.hh"
 #include "replica/database.hh"
+#include "db/config.hh"
 
 using namespace std::string_literals;
 
@@ -83,19 +84,21 @@ void cql3::statements::alter_keyspace_statement::validate(query_processor& qp, c
                         current_options.type_string(), new_options.type_string()));
             }
 
-            auto new_ks = _attrs->as_ks_metadata_update(ks.metadata(), *qp.proxy().get_token_metadata_ptr(), qp.proxy().features());
+            const auto tmptr = qp.proxy().get_token_metadata_ptr();
+            auto new_ks = _attrs->as_ks_metadata_update(ks.metadata(), *tmptr, qp.proxy().features());
 
             if (ks.get_replication_strategy().uses_tablets()) {
                 const std::map<sstring, sstring>& current_rf_per_dc = ks.metadata()->strategy_options();
                 auto new_rf_per_dc = _attrs->get_replication_options();
                 new_rf_per_dc.erase(ks_prop_defs::REPLICATION_STRATEGY_CLASS_KEY);
+
                 unsigned total_abs_rfs_diff = 0;
                 for (const auto& [new_dc, new_rf] : new_rf_per_dc) {
                     int old_rf_value = 0;
                     if (auto new_dc_in_current_mapping = current_rf_per_dc.find(new_dc);
                              new_dc_in_current_mapping != current_rf_per_dc.end()) {
                         old_rf_value = parse_rf(new_dc_in_current_mapping->second);
-                    } else if (!qp.proxy().get_token_metadata_ptr()->get_topology().get_datacenters().contains(new_dc)) {
+                    } else if (!tmptr->get_topology().get_datacenters().contains(new_dc)) {
                         // This means that the DC listed in ALTER doesn't exist. This error will be reported later,
                         // during validation in abstract_replication_strategy::validate_replication_strategy.
                         // We can't report this error now, because it'd change the order of errors reported:
@@ -108,6 +111,35 @@ void cql3::statements::alter_keyspace_statement::validate(query_processor& qp, c
 
                     if (total_abs_rfs_diff += rf_diff; total_abs_rfs_diff >= 2) {
                         throw exceptions::invalid_request_exception("Only one DC's RF can be changed at a time and not by more than 1");
+                    }
+                }
+
+                const auto& rack_map = tmptr->get_topology().get_datacenter_racks();
+
+                // We cannot allow for ALTER to proceed if it would lead to an RF-rack-invalid keyspace.
+                // For more context, see: scylladb/scylladb#23276.
+                for (const auto& [new_dc, rf] : new_rf_per_dc) {
+                    const auto rf_value = parse_rf(rf);
+                    size_t normal_rack_count = 0;
+                    for (const auto& [_, rack_nodes] : rack_map.at(new_dc)) {
+                        // We must ignore zero-token nodes because they don't take part in replication.
+                        // Verify that this rack has at least one normal node.
+                        const bool normal_rack = std::ranges::any_of(rack_nodes, [tmptr] (locator::host_id host_id) {
+                            return tmptr->is_normal_token_owner(host_id);
+                        });
+                        if (normal_rack) {
+                            ++normal_rack_count;
+                        }
+                    }
+                    const bool restricted_keyspaces = qp.db().get_config().rf_rack_valid_keyspaces();
+                    const bool invalid_rf = (rf_value != (int) normal_rack_count) && rf_value != 1 && rf_value != 0;
+
+                    if (restricted_keyspaces && invalid_rf) {
+                        throw exceptions::invalid_request_exception(seastar::format(
+                                "The option `rf_rack_valid_keyspaces` is enabled. It requires that keyspaces remain RF-rack-valid. "
+                                "Your query would violate that: keyspace '{}' uses tablets and the condition is not satisfied for DC '{}': "
+                                "RF={} vs. rack count={}.",
+                                _name, new_dc, rf_value, normal_rack_count));
                     }
                 }
             }
