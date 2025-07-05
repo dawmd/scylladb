9bf0f4a29a4b26cb5de2fe3f19061a0378a35946
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-07-05 21:18:17 +0200

    introduce index_options

---
+++ index/index_options.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ *
+ * Modified by ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (LicenseRef-ScyllaDB-Source-Available-1.0 and Apache-2.0)
+ */
+
+#include "index/index_options.hh"
+#include "utils/log.hh"
+
+#include <seastar/core/on_internal_error.hh>
+
+#include <string_view>
+#include <utility>
+#include <vector>
+
+namespace secondary_index {
+
+namespace {
+
+logging::logger logger{"index_options"};
+
+// A map of valid index options. It does NOT contain any option that is valid for the underlying MV.
+// Those are supposed to be stored somewhere else.
+//
+// FIXME: Change to std::flat_map.
+const std::vector<std::pair<index_type, std::vector<std::string_view>>> valid_index_options = {
+    {index_type::regular, {
+        // None.
+    }},
+    {index_type::vector, {
+        "similarity_function",
+        "maximum_node_connections",
+        "construction_beam_width",
+        "search_beam_width"
+    }}
+};
+
+std::span<const std::string_view> get_index_options(index_type type) {
+    for (auto&& [index_type, index_opts] : valid_index_options) {
+        if (index_type == type) {
+            return index_opts;
+        }
+    }
+
+    // Casting to size_t to avoid having to deal with formatting one-byte types...
+    size_t enum_value = std::to_underlying(type);
+    on_internal_error(logger, seastar::format("There are no options for index_type of enum value equal to {}", enum_value));
+}
+
+} // anonymous namespace
+
+std::expected<index_options, sstring> filter_options(index_type type, const std::unordered_map<sstring, sstring>& options) {
+    index_options result {.type = type};
+
+    const auto index_opts = get_index_options(type);
+    for (const auto& [key, value] : options) {
+        if (std::ranges::find(index_opts, key) == std::ranges::end(index_opts)) {
+            return std::unexpected(key);
+        }
+
+        result.idx_options.emplace(std::make_pair(key, value));
+    }
+
+    return result;
+}
+
+} // namespace secondary_index
---
+++ index/index_options.hh
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ *
+ * Modified by ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (LicenseRef-ScyllaDB-Source-Available-1.0 and Apache-2.0)
+ */
+
+#pragma once
+
+#include <seastar/core/sstring.hh>
+
+#include <expected>
+#include <map>
+#include <unordered_map>
+
+using namespace seastar;
+
+namespace secondary_index {
+
+enum class index_type {
+    regular,    // A non-custom, global or local secondary index, with an underlying MV.
+    vector      // Custom vector index.
+};
+
+/// Depending on the type of an index, it's configured by different options.
+///
+/// Example 1: vector index accepts `similarity_function` as one of its options,
+///            but a regular global secondary index does not.
+/// Example 2: a regular global secondary index will accept `synchronous_updates`
+///            as its option (and apply it to its underlying materialized view),
+///            but it's invalid for vector index as it doesn't utilize MVs in any way.
+///
+/// This struct is supposed to be a wrapper over filtered options. The goal is to provide
+/// a convenient access to them for validation reasons, e.g. when processing a request to
+/// create or alter an index.
+struct index_options {
+    index_type type;
+    // Options corresponding to the index itself.
+    // The map should contain all valid options that are applicable to an index directly.
+    // It should NOT contain ANY option that is only applicable to the underlying MV.
+    std::map<sstring, sstring> idx_options;
+    // Options corresponding to the underlying MV of the index.
+    std::map<sstring, sstring> mv_options;
+};
+
+// Return filtered options. If at least one of the options is invalid, return its name.
+//
+// FIXME: It would be nice to accept a broader set of ranges here.
+std::expected<index_options, sstring> filter_options(index_type, const std::unordered_map<sstring, sstring>& options);
+
+} // namespace secondary_index
