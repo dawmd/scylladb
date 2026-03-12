33dd80eec5c47def61bcb45efd302b8405dd0b41
Dawid Mędrek <dawid.medrek@scylladb.com>
2026-03-12 19:14:35 +0100

    service: strong_consistency: Fix indentation
    We fix the indentation issues introduced in the previous commit.

--- service/strong_consistency/coordinator.cc
+++ service/strong_consistency/coordinator.cc
@@ -118,72 +118,72 @@ future<value_or_redirect<>> coordinator::mutate(schema_ptr schema,
     auto aoe = abort_on_expiry<timeout_clock>(timeout);
 
     try {
-    auto op_result = co_await create_operation_ctx(*schema, token, aoe.abort_source());
-    if (const auto* redirect = get_if<need_redirect>(&op_result)) {
-        co_return *redirect;
-    }
-    auto& op = get<operation_ctx>(op_result);
+        auto op_result = co_await create_operation_ctx(*schema, token, aoe.abort_source());
+        if (const auto* redirect = get_if<need_redirect>(&op_result)) {
+            co_return *redirect;
+        }
+        auto& op = get<operation_ctx>(op_result);
 
-    while (true) {
-        auto disposition = op.raft_server.begin_mutate(aoe.abort_source(), timeout);
-        if (const auto* not_a_leader = get_if<raft::not_a_leader>(&disposition)) {
-            const auto leader_host_id = locator::host_id{not_a_leader->leader.uuid()};
-            const auto* target = find_replica(op.tablet_info, leader_host_id);
-            if (!target) {
-                on_internal_error(logger,
-                    ::format("table {}.{}, tablet {}, current leader {} is not a replica, replicas {}",
-                        schema->ks_name(), schema->cf_name(), op.tablet_id, 
-                        leader_host_id, op.tablet_info.replicas));
+        while (true) {
+            auto disposition = op.raft_server.begin_mutate(aoe.abort_source(), timeout);
+            if (const auto* not_a_leader = get_if<raft::not_a_leader>(&disposition)) {
+                const auto leader_host_id = locator::host_id{not_a_leader->leader.uuid()};
+                const auto* target = find_replica(op.tablet_info, leader_host_id);
+                if (!target) {
+                    on_internal_error(logger,
+                        ::format("table {}.{}, tablet {}, current leader {} is not a replica, replicas {}",
+                            schema->ks_name(), schema->cf_name(), op.tablet_id,
+                            leader_host_id, op.tablet_info.replicas));
+                }
+                co_return need_redirect{*target};
             }
-            co_return need_redirect{*target};
-        }
-        if (auto* wait_for_leader = get_if<raft_server::need_wait_for_leader>(&disposition)) {
-            co_await std::move(wait_for_leader->future);
-            continue;
-        }
-        const auto [ts, term] = get<raft_server::timestamp_with_term>(disposition);
-
-        const raft_command command {
-            .mutation{mutation_gen(ts)}
-        };
-        raft::command raft_cmd;
-        ser::serialize(raft_cmd, command);
-
-        logger.debug("mutate(): add_entry({}), term {}",
-            command.mutation.pretty_printer(schema), term);
-        try {
-            co_await op.raft_server.server().add_entry(std::move(raft_cmd),
-                raft::wait_type::committed,
-                &aoe.abort_source());
-            co_return std::monostate{};
-        } catch (...) {
-            auto ex = std::current_exception();
-            if (try_catch<raft::stopped_error>(ex)) {
-                // Holding raft_server.holder guarantees that the raft::server is not
-                // aborted until the holder is released.
-
-                on_internal_error(logger,
-                    format("mutate(): add_entry, unexpected exception {}, table {}.{}, tablet {}, term {}", 
-                        ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term));
-            } else if (try_catch<raft::not_a_leader>(ex) || try_catch<raft::dropped_entry>(ex)) {
-                logger.debug("mutate(): add_entry, got retriable error {}, table {}.{}, tablet {}, term {}",
-                    ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term);
-
+            if (auto* wait_for_leader = get_if<raft_server::need_wait_for_leader>(&disposition)) {
+                co_await std::move(wait_for_leader->future);
                 continue;
-            } else if (try_catch<raft::commit_status_unknown>(ex)) {
-                logger.debug("mutate(): add_entry, got commit_status_unknown {}, table {}.{}, tablet {}, term {}",
-                    ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term);
-
-                // FIXME: use a dedicated ERROR_CODE instead of SERVER_ERROR
-                throw exceptions::server_exception(
-                    "The outcome of this statement is unknown. It may or may not have been applied. "
-                    "Retrying the statement may be necessary.");
             }
+            const auto [ts, term] = get<raft_server::timestamp_with_term>(disposition);
 
-            // Let the outer code handle other errors.
-            throw;
+            const raft_command command {
+                .mutation{mutation_gen(ts)}
+            };
+            raft::command raft_cmd;
+            ser::serialize(raft_cmd, command);
+
+            logger.debug("mutate(): add_entry({}), term {}",
+                command.mutation.pretty_printer(schema), term);
+            try {
+                co_await op.raft_server.server().add_entry(std::move(raft_cmd),
+                    raft::wait_type::committed,
+                    &aoe.abort_source());
+                co_return std::monostate{};
+            } catch (...) {
+                auto ex = std::current_exception();
+                if (try_catch<raft::stopped_error>(ex)) {
+                    // Holding raft_server.holder guarantees that the raft::server is not
+                    // aborted until the holder is released.
+
+                    on_internal_error(logger,
+                        format("mutate(): add_entry, unexpected exception {}, table {}.{}, tablet {}, term {}",
+                            ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term));
+                } else if (try_catch<raft::not_a_leader>(ex) || try_catch<raft::dropped_entry>(ex)) {
+                    logger.debug("mutate(): add_entry, got retriable error {}, table {}.{}, tablet {}, term {}",
+                        ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term);
+
+                    continue;
+                } else if (try_catch<raft::commit_status_unknown>(ex)) {
+                    logger.debug("mutate(): add_entry, got commit_status_unknown {}, table {}.{}, tablet {}, term {}",
+                        ex, schema->ks_name(), schema->cf_name(), op.tablet_id, term);
+
+                    // FIXME: use a dedicated ERROR_CODE instead of SERVER_ERROR
+                    throw exceptions::server_exception(
+                        "The outcome of this statement is unknown. It may or may not have been applied. "
+                        "Retrying the statement may be necessary.");
+                }
+
+                // Let the outer code handle other errors.
+                throw;
+            }
         }
-    }
     } catch (...) {
         auto ex = std::current_exception();
         // Unfortunately, timeouts can materialize in different forms depending
@@ -218,18 +218,18 @@ auto coordinator::query(schema_ptr schema,
     auto aoe = abort_on_expiry<timeout_clock>(timeout);
 
     try {
-    auto op_result = co_await create_operation_ctx(*schema, ranges[0].start()->value().token(), aoe.abort_source());
-    if (const auto* redirect = get_if<need_redirect>(&op_result)) {
-        co_return *redirect;
-    }
-    auto& op = get<operation_ctx>(op_result);
+        auto op_result = co_await create_operation_ctx(*schema, ranges[0].start()->value().token(), aoe.abort_source());
+        if (const auto* redirect = get_if<need_redirect>(&op_result)) {
+            co_return *redirect;
+        }
+        auto& op = get<operation_ctx>(op_result);
 
-    co_await op.raft_server.server().read_barrier(&aoe.abort_source());
+        co_await op.raft_server.server().read_barrier(&aoe.abort_source());
 
-    auto [result, cache_temp] = co_await _db.query(schema, cmd,
-        query::result_options::only_result(), ranges, trace_state, timeout);
+        auto [result, cache_temp] = co_await _db.query(schema, cmd,
+            query::result_options::only_result(), ranges, trace_state, timeout);
 
-    co_return std::move(result);
+        co_return std::move(result);
     } catch (...) {
         auto ex = std::current_exception();
         // Unfortunately, timeouts can materialize in different forms depending
